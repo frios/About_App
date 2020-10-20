06a10fdc0680bd6ce6b7a8a9210a573bd17b18a0
Ferdinand Rios <frios@viciouscircle.com>
2020-10-20 14:05:09 -0700

    test

--- Sources/About_App/HoverWithPaddingModifier.swift
+++ Sources/About_App/HoverWithPaddingModifier.swift
@@ -11,8 +11,7 @@ public struct HoverWithPaddingModifier: ViewModifier {
     public func body(content: Content) -> some View {
         content
             .padding(6)
-//            .background(Color.clear)
-            .contentShape(RoundedRectangle(cornerRadius: 5, style: .continuous))
+//            .contentShape(RoundedRectangle(cornerRadius: 5, style: .continuous))
             .hoverEffect(.highlight)
     }
 }
